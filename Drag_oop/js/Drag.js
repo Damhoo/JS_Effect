0359b0179d653a6b94473d4703bbbbb75b46e9ce
zlsman <shitsui@sina.com>
2019-11-22 23:14:26 +0800

    修改了拖拽效果

--- Drag_oop/js/Drag.js
+++ Drag_oop/js/Drag.js
@@ -5,6 +5,7 @@ function Drag(id) {
 	this.oDrag = document.getElementById(id);
 	this.oDrag.onmousedown = function (ev) {
 		_this.fnDown(ev);
+		return false;
 	};
 }
 Drag.prototype.fnDown = function (ev) {
@@ -12,19 +13,22 @@ Drag.prototype.fnDown = function (ev) {
 	var _this = this;
 	this.disX = oEvent.clientX - this.oDrag.offsetLeft;
 	this.disY = oEvent.clientY - this.oDrag.offsetTop;
-	document.onmousemove = function (ev) {
-		_this.fnMove(ev);
+	if (_this.oDrag.setCapture) {
+		_this.oDrag.onmousemove = function (ev) {_this.fnMove(ev);}
+		_this.oDrag.onmouseup = function () {_this.fnUp(this);};
+		_this.oDrag.setCapture();
+	} else {
+		document.onmousemove = function (ev) {_this.fnMove(ev);}
+		document.onmouseup = function () {_this.fnUp(this);};
 	}
-	document.onmouseup = function () {
-		_this.fnUp();
-	};
 };
 Drag.prototype.fnMove = function (ev) {
 	var oEvent = ev || event;
 	this.oDrag.style.left = oEvent.clientX - this.disX+'px';
 	this.oDrag.style.top = oEvent.clientY - this.disY+'px';
 };
-Drag.prototype.fnUp = function () {
-	document.onmousemove = null;
-	document.onmouseup = null;
+Drag.prototype.fnUp = function (obj) {
+	obj.onmousemove = null;
+	obj.onmouseup = null;
+	if (this.oDrag.releaseCapture) {this.oDrag.releaseCapture();}
 };
